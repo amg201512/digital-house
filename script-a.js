81e1822b979200031212e5f4f0074087f94cfeca
amg201512 <amg201512@gmail.com>
2022-03-01 21:42:23 -0500

    ejercicio pg

--- script-a.js
+++ script-a.js
@@ -1,1 +1,2 @@
 console.log("Este es el contenido del script A")
+console.log ("contenido modificado por amg201512")
--- script-b.js
+++ script-b.js
@@ -1,1 +1,2 @@
 console.log("Este es el contenido del script B")
+console.log ("El contenido ya fue modificado por amg201512")
